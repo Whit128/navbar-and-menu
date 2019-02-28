5e0b377fd123e011aef5bed79b5184b651b5c076
Dave <contact@davidwhitcome.com>
2019-02-27 19:04:04 -0600

    animate.js renamed to menu.js

---
+++ menu.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Menu
+    var menu__open = 0;
+    function menu__toggle() {
+        
+        if (menu__open == 0 && window.getComputedStyle(document.getElementById("page__fade")).opacity == '0') {
+            document.getElementById("hamburger__menu").style.overflowY = 'auto';
+            document.body.style.overflowY = 'hidden';
+            document.getElementById("hamburger__menu").style.width = '250px';
+            document.getElementById("page__fade").style.display = 'block';
+            document.getElementById("navbar").style.marginTop = '0';
+            setTimeout( // makes sure that CSS3 animation plays 
+                function() {
+                    document.getElementById("page__fade").style.opacity = '1';
+                }, 0);
+            menu__open=1;
+        }
+        else if (menu__open == 1  && window.getComputedStyle(document.getElementById("page__fade")).opacity == '1') {
+            document.getElementById("hamburger__menu").style.overflowY = 'hidden';
+            document.body.style.overflowY = 'auto';
+            document.getElementById("hamburger__menu").style.width = '0';
+            document.getElementById("page__fade").style.opacity = '0';
+
+            setTimeout(
+                function() {
+                    document.getElementById("page__fade").style.display = 'none';
+                }, 200);
+            menu__open=0;
+        }
+        return false;
+    }
+
+    document.getElementById("menu__button").onclick = function(){
+        menu__toggle();
+    };
+
+    var elements = document.getElementById("hamburger__menu").getElementsByTagName('a')
+
+    var n;
+    for (n = 0; n < elements.length; ++n)
+        elements[n].onclick = function(){
+            if (window.innerWidth <= 760)
+                menu__toggle();
+        };
+ 
+
+    document.getElementById("page__fade").onclick = function(){
+        menu__toggle();
+    };
+});
+
+// // Scroll smoothly when pressing links on navbar
+// $(document).on('click', 'a', function(event){
+//     if ($(this).hasClass("top__button")) {
+//         $('html,body').stop().animate({ scrollTop: 0 }, 500);
+//     } else {
+//         $('html, body').stop().animate({
+//             scrollTop: $($.attr(this, 'href')).offset().top
+//         }, 500);                    
+//     }
+//     return false;
+// });
